fd9583007856f6ac7143be178ce3ae65a7af7efd
MKULTRA001 <tim.salazar001@gmail.com>
2023-02-10 09:26:50 -0700

    initial commit

---
+++ src/main/java/cse326/SoftwareEng/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package cse326.SoftwareEng.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
