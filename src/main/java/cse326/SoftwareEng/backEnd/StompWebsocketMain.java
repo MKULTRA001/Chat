3e75b5e32c3da5f82b36f89ac07d645c6b9c5511
Zjwghg-2 <matthew.beecher@student.nmt.edu>
2023-02-17 16:14:08 -0700

    Added temporary html and main runnable for websocket test

---
+++ src/main/java/cse326/SoftwareEng/backEnd/StompWebsocketMain.java
@@ -0,0 +1,11 @@
+package cse326.SoftwareEng.backEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompWebsocketMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
--- src/main/java/cse326/SoftwareEng/backEnd/testLogic.js
+++ src/main/java/cse326/SoftwareEng/backEnd/testLogic.js
@@ -1,18 +1,3 @@
-/*
-This file assumes a few things, taken from the Spring setup framework's HTML file and associated js socket logic
-1: there is a connect button with id "connect"
-2: there is a disconnect button with id "disconnect"
-3: there is an input field with id "input"
-4: there is a send message button with id "send"
-5: there is a table with tbody id "text"
-If you look at the html code at https://spring.io/guides/gs/messaging-stomp-websocket/ you'll see these w/ different names:
-    text is called greetings
-    input is called name
-they also have an extra function to toggle buttons and stuff, but overall the structure between the two is the same.
-
-The example also has 2 message container classes, which I consider unnecessary, so I didn't do that.
- */
-
 var stompClient = null;
 
 function connect() {
