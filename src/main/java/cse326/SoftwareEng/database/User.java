ecbdf6da3446dc2687b7618ed3c4191174c7d345
DistantValhalla <Johnathan.Martinez@student.nmt.edu>
2023-02-21 10:54:53 -0700

    Comments on databases/User.java

--- src/main/java/cse326/SoftwareEng/database/User.java
+++ src/main/java/cse326/SoftwareEng/database/User.java
@@ -1,15 +1,24 @@
+/**
+ * Directly called to manifest the MYSQL table "user" in the database "user_info". It is able to define constraints and rename elements of the table using Spring's own syntax
+ * Note: If the table "user" does not exist, upon running Spring the table will automatically be generated with the specified parameters below
+ *
+ *
+ */
+
+
 package cse326.SoftwareEng.database;
-
 import jakarta.persistence.*;
-
 import java.util.Date;
 
+
+/*Designates this file as an entity and assigns the table with the name "user" */
 @Entity
 @Table(name = "user")
 public class User {
-
+    /*Assigns "user_id" as the primary key for this table. It will be generated automatically and is currently set to increment with each new user*/
     @Id
     @GeneratedValue(generator="increment")
+    /*You are able to keep the variable name as the column name in the database or choose to assign it yourself. It will be automatically be updated. This field is also where MYSQL constraints can be defined */
     @Column(name = "user_id")
     private int id;
 
@@ -23,6 +32,7 @@ public class User {
     private String username;
 
     @Column(name = "created_at",updatable = false)
+    /*Temporal is used to annotate a Java date with TIMESTAMP corresponding to the data and time*/
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
     @Column(name = "updated_at")
@@ -34,6 +44,10 @@ public class User {
         return id;
     }
 
+    /**
+     * Used to set a user id. Since it is the primary key of the table, it must be unique.
+     * @param id user id
+     */
     public void setId(int id) {
         this.id = id;
     }
@@ -42,6 +56,10 @@ public class User {
         return email;
     }
 
+    /**
+     * Used to set a user email. It is specified in its constraints that the email must be unique
+     * @param email user email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
@@ -74,6 +92,10 @@ public class User {
         return updated_at;
     }
 
+    /**
+     * Date is in YYYY-MM-DD format and a time in the HH:MM:SS.ssssss format
+     * @param updated_at time password was changed
+     */
     public void setUpdated_at(Date updated_at) {
         this.updated_at = updated_at;
     }
