e4af72b92596a86ab8cad339e5b21496a63ecbc9
MKULTRA001 <tim.salazar001@gmail.com>
2023-03-06 13:23:20 -0700

    Main is now chatApplication in the package chat

--- src/main/java/cse326/SoftwareEng/chat/ChatApplication.java
+++ src/main/java/cse326/SoftwareEng/chat/ChatApplication.java
@@ -3,7 +3,7 @@ package cse326.SoftwareEng.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cse326.SoftwareEng.database"})
 public class ChatApplication {
 
 	public static void main(String[] args) {
