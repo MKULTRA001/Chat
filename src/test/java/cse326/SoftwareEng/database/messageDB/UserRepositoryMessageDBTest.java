2b0c6b0b4d02da309013484e73905b6ae8d79e6a
MKULTRA001 <tim.salazar001@gmail.com>
2023-03-30 10:14:27 -0600

    Fixed an issue with unit testing

---
+++ src/test/java/cse326/SoftwareEng/database/messageDB/UserRepositoryMessageDBTest.java
@@ -0,0 +1,12 @@
+package cse326.SoftwareEng.database.messageDB;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryMessageDBTest {
+
+    @Test
+    void findByUsername() {
+    }
+}
